1c95f4d3aae9ca7b822cdb08a5acb016fb317e21
noelweichbrodt <noel@weichbrodt.org>
2010-12-02 00:49:07 -0600

    Fixed import for obsTime with associated changes; fixed glmfit models by removing sst from estimators.

--- Analysis/drifterDataExplodeColumns.m
+++ Analysis/drifterDataExplodeColumns.m
@@ -1,12 +1,14 @@
-function [platID obsLat obsLong sst ewCurrent nsCurrent eLat eLong expNum wmo drogue] = drifterDataExplodeColumns(data)
+function [platID obsTime obsLat obsLong sst ewCurrent nsCurrent eLat eLong expNum wmo drogue] = ...
+    drifterDataExplodeColumns(data)
 platID=data(:,1);
-obsLat=data(:,2);
-obsLong=data(:,3);
-sst=data(:,4);
-ewCurrent=data(:,5);
-nsCurrent=data(:,6);
-eLat=data(:,7);
-eLong=data(:,8);
-expNum=data(:,9);
-wmo=data(:,10);
-drogue=data(:,11);
+obsTime=data(:,2);
+obsLat=data(:,3);
+obsLong=data(:,4);
+sst=data(:,5);
+ewCurrent=data(:,6);
+nsCurrent=data(:,7);
+eLat=data(:,8);
+eLong=data(:,9);
+expNum=data(:,10);
+wmo=data(:,11);
+drogue=data(:,12);
--- Analysis/drifter_single_var_foo.m
+++ Analysis/drifter_single_var_foo.m
@@ -1,21 +1,40 @@
 %Read in data
-[platID2007 obsLat2007 obsLong2007 sst2007 ewCurrent2007 nsCurrent2007 eLat2007 eLong2007 expNum2007 wmo2007 drogue2007]=drifterDataImport('Data/2007_k_arc.csv');
-arc2007 = [platID2007 obsLat2007 obsLong2007 sst2007 ewCurrent2007 nsCurrent2007 eLat2007 eLong2007 expNum2007 wmo2007 drogue2007];
+[platID2007 obsTime2007 obsLat2007 obsLong2007 sst2007 ewCurrent2007 ... 
+    nsCurrent2007 eLat2007 eLong2007 expNum2007 wmo2007 drogue2007] = ... 
+    drifterDataImport('Data/2007_k_arc.csv');
+arc2007 = [platID2007 obsTime2007 obsLat2007 obsLong2007 sst2007 ...
+    ewCurrent2007 nsCurrent2007 eLat2007 eLong2007 expNum2007 wmo2007 drogue2007];
 arc2007Clean = drifterDataClean(arc2007);
-[platID2007Clean obsLat2007Clean obsLong2007Clean sst2007Clean ewCurrent2007Clean nsCurrent2007Clean eLat2007Clean eLong2007Clean expNum2007Clean wmo2007Clean drogue2007Clean]=drifterDataExplodeColumns(arc2007Clean);
-[platID2008 obsLat2008 obsLong2008 sst2008 ewCurrent2008 nsCurrent2008 eLat2008 eLong2008 expNum2008 wmo2008 drogue2008]=drifterDataImport('Data/2008_k_arc.csv');
-arc2008 = [platID2008 obsLat2008 obsLong2008 sst2008 ewCurrent2008 nsCurrent2008 eLat2008 eLong2008 expNum2008 wmo2008 drogue2008];
+[platID2007Clean obsTime2007Clean obsLat2007Clean obsLong2007Clean sst2007Clean ... 
+    ewCurrent2007Clean nsCurrent2007Clean eLat2007Clean eLong2007Clean ...
+    expNum2007Clean wmo2007Clean drogue2007Clean]=drifterDataExplodeColumns(arc2007Clean);
+[platID2008 obsTime2008 obsLat2008 obsLong2008 sst2008 ewCurrent2008 nsCurrent2008 ...
+    eLat2008 eLong2008 expNum2008 wmo2008 drogue2008]=drifterDataImport('Data/2008_k_arc.csv');
+arc2008 = [platID2008 obsTime2008 obsLat2008 obsLong2008 sst2008 ewCurrent2008 ... 
+    nsCurrent2008 eLat2008 eLong2008 expNum2008 wmo2008 drogue2008];
 arc2008Clean = drifterDataClean(arc2008);
-[platID2008Clean obsLat2008Clean obsLong2008Clean sst2008Clean ewCurrent2008Clean nsCurrent2008Clean eLat2008Clean eLong2008Clean expNum2008Clean wmo2008Clean drogue2008Clean]
+[platID2008Clean obsTime2008Clean obsLat2008Clean obsLong2008Clean sst2008Clean ...
+    ewCurrent2008Clean nsCurrent2008Clean eLat2008Clean eLong2008Clean ...
+    expNum2008Clean wmo2008Clean drogue2008Clean] = drifterDataExplodeColumns(arc2008Clean);
 
 %Create subsets of data for analysis
 length(drogue2007)-length(drogue2007Clean)
-arc2007doff=arc2007Clean(logical(~drogue2007Clean),:);
+
+arc2007DOff=arc2007Clean(logical(~drogue2007Clean),:);
+arc2007PredictorsCleanDOff=cat(2,arc2007DOff(:,1:4),arc2007DOff(:,6:12));
+arc2007ResponseCleanDOff = arc2007DOff(:,5);
+
+arc2007DOn=arc2007Clean(logical(drogue2007Clean),:);
+arc2007DOnPredictorsClean=cat(2,arc2007DOn(:,1:4),arc2007DOn(:,6:12));
+arc2007DOnResponseClean = arc2007DOn(:,5);
+
+arc2007DonRaw=arc2007(logical(drogue2007),:);
+arc2007DOnPredictorsRaw=cat(2,arc2007DonRaw(:,1:4),arc2007DonRaw(:,6:12));
+arc2007DOnResponseRaw=arc2007DonRaw(:,5);
+
 arc2007don=arc2007Clean(logical(drogue2007Clean),:);
 arc2007dondrogue=arc2007Clean(logical(drogue2007Clean),11);
 arc2007doffdrogue=arc2007Clean(logical(~drogue2007Clean),11);
-arc2007donsst=arc2007Clean(logical(drogue2007Clean),4);
-arc2007doffsst=arc2007Clean(logical(~drogue2007Clean),4);
 arc2007RawDOn=arc2007(logical(drogue2007),:);
 arc2007RawDOnSST=arc2007(logical(drogue2007),4);
 arc2008DOnSST=arc2008Clean(logical(~arc2008Clean(:,11)),4);
@@ -42,19 +61,19 @@ drogueoffstats.p
 drogueoffstats.coeffcorr
 drogueoffstats.se
 
-[sstDonFit,sstDOnDev,sstDOnStats]=glmfit(arc2007don,arc2007donsst,'normal');
+[sstDonFit,sstDOnDev,sstDOnStats]=glmfit(arc2007DOnPredictorsClean,arc2007DOnResponseClean,'normal');
 sstDonFit
 sstDOnStats.p
 sstDOnStats.coeffcorr
 sstDOnStats.se
 
-[sstdofffit,sstDOffDev,sstdoffstats]=glmfit(arc2007doff,arc2007doffsst,'normal');
+[sstdofffit,sstDOffDev,sstdoffstats]=glmfit(arc2007PredictorsCleanDOff,arc2007ResponseCleanDOff,'normal');
 sstdofffit
 sstdoffstats.p
 sstdoffstats.coeffcorr
 sstdoffstats.se
 
-[sstRawDOnFit,sstDOnDev,sstRawDOnStats]=glmfit(arc2007RawDOn,arc2007RawDOnSST,'normal');
+[sstRawDOnFit,sstDOnDev,sstRawDOnStats]=glmfit(arc2007DOnPredictorsRaw,arc2007DOnResponseRaw,'normal');
 sstRawDOnFit
 sstRawDOnStats.p
 sstRawDOnStats.coeffcorr
@@ -82,6 +101,12 @@ plot(1:12,sstDOnStats.p,1:12,sstdoffstats.p,1:12,sstRawDOnStats.p)
 xlabel('coefficients')
 ylabel('p-values in glmfit')
 title('p-values for 12 data vectors with drogue on/off, sst, and raw/clean analysis') 
+legend('Clean Regress on SST with Drogue On','Clean regress on SST with Drogue Off','Raw regress on SST with Drogue On','Location','NorthWest')
+
+plot(1:12,sstDonFit,1:12,sstdofffit,1:12,sstRawDOnFit)
+xlabel('coefficients')
+ylabel('betas from glmfit')
+title('beta estimates for sst using 11 data vectors with drogue on/off, and raw/clean analysis') 
 legend('Clean Regress on SST with Drogue On','Clean regress on SST with Drogue Off','Raw regress on SST with Drogue On','Location','NorthWest')
 
 plot(1:12,avgGLMPValues)
